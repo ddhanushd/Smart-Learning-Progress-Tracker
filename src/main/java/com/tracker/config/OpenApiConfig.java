c845b74bbf3f12035bd1bbe66c32b20327589ade
ddhanushd <dhanushdevaraj17@gmail.com>
2025-12-20 14:06:32 +0530

    secured swagger with jwt

--- src/main/java/com/tracker/config/OpenApiConfig.java
+++ src/main/java/com/tracker/config/OpenApiConfig.java
@@ -1,18 +1,27 @@
 package com.tracker.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Smart Learning Progress Tracker API",
+                version = "v1",
+                description = "Backend APIs for tracking past revisions, current learning progress, and future study planning using confidence scores, deadlines, and analytics and JWT secured"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
-    @Bean
-    public OpenAPI learningTrackerApi() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Smart Learning Tracker API")
-                        .description("Backend API for tracking learning progress, revisions, deadlines and analytics")
-                        .version("v1.0"));
-    }
+
 }
